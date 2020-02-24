df431a7e4004dea80dcf20e0ba7b34dfbf38d45e
AzureGem <rreminy@gmail.com>
2020-02-24 16:10:21 -0500

    Add the Webhook HTTP Server enabled logic

--- start.py
+++ start.py
@@ -3,7 +3,9 @@ from filobot.tasks import update_game, update_hunts, start_server, track_stats
 
 bot.loop.create_task(update_hunts())
 bot.loop.create_task(update_game())
-bot.loop.create_task(start_server())
 bot.loop.create_task(track_stats())
+
+if config.get('WebhookServer', 'Enabled') == 'TRUE':
+    bot.loop.create_task(start_server(config.get('WebhookServer', 'Address'), config.get('WebhookServer', 'Port')))
 
 bot.run(config.get('Bot', 'Token'))
